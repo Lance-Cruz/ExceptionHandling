dc34020276251e4a463aa65961cbd95b304d3ee2
Lance Cruz <G00428876@atu.ie>
2024-10-28 13:01:04 +0000

    Fixed to keep asking after invalid number

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9 :");
-        try(Scanner sc = new Scanner(System.in)) {
+        try (Scanner sc = new Scanner(System.in)) {
             boolean active = true;
-            try {
-                while (active) {
-                    String input = sc.nextLine();
-
+            while (active) {
+                String input = sc.nextLine();
+                try {
                     int myNum = Integer.parseInt(input);
 
                     if (myNum >= 0 && myNum <= 9) {
                         System.out.println("You entered " + myNum);
                         active = false;
                     } else {
-                        System.out.println("Please enter a number between 0 - 9 .");
+                        System.out.println("Please enter a number between 0 - 9.");
                     }
-                }
-            }
-            catch(NumberFormatException ae){
+                } catch (NumberFormatException ae) {
                     System.out.println("Not a valid number, please enter a number between 0 - 9.");
+                }
             }
         }
     }
